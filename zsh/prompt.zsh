a498ddda9493ab44461581f3178772b6cc43d5f1
delianides <drew@delianides.com>
2014-06-29 08:25:46 -0400

    added abstracted prompt file

---
+++ zsh/prompt.zsh
@@ -0,0 +1,3 @@
+local ret_status="%(?:%{$fg_bold[green]%}☺ :%{$fg_bold[red]%}☹ %s)%{$reset_color%}"
+
+export PS1='${SSH_CONNECTION+"%{$fg_bold[green]%}%n@%m:"}%{$fg_bold[blue]%}%c%{$reset_color%}$(git_prompt_info) ${ret_status}%'
