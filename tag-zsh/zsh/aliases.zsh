6f065cb3ee82e97cb346dff8fce696a1c715828b
delianides <drew@delianides.com>
2016-05-26 17:14:10 -0400

    added tag zsh

---
+++ tag-zsh/zsh/aliases.zsh
@@ -0,0 +1,44 @@
+###########
+# ALIASES #
+###########
+alias qq="cd . && source ~/.zshrc"
+alias cp="cp -iv"
+alias rm="rm -iv"
+alias mv="mv -iv"
+alias ls="ls -FGh"
+alias du="du -cksh"
+alias df="df -h"
+# Use modern regexps for sed, i.e. "(one|two)", not "\(one\|two\)"
+alias sed="sed -E"
+
+# Use modern regexps for grep, and do show color when `grep` is the final
+# command, but don't when piping to something else, because the added color
+# codes will mess up the expected input.
+alias grep="egrep --color=auto"
+
+# Copy-pasting `$ python something.py` works
+alias \$=''
+alias diff="colordiff -u"
+alias mkdir="\mkdir -p"
+
+alias prettyjson="python -m json.tool"
+alias prettyxml="xmllint --format -"
+
+# Needs to be a function because `alias -` breaks
+function -() { cd - }
+
+# Global aliases
+alias -g G="| grep "
+alias -g ONE="| awk '{ print \$1}'"
+
+# Psh, "no nodename or servname not provided". I'll do `whois
+# http://google.com/hello` if I want.
+function whois() {
+	command whois $(echo "$1" | sed -E -e 's|^https?://||' -e 's|/.*$||g')
+}
+
+function al { ls -t | head -n ${1:-10}; }
+
+# If piping something in, copy it.
+# If just doing `clip`, paste it.
+function clip { [ -t 0 ] && pbpaste || pbcopy;}
---
+++ tag-zsh/zsh/editor.zsh
@@ -0,0 +1,7 @@
+############
+#  EDITOR  #
+############
+
+# Why set $VISUAL instead of $EDITOR?
+# http://robots.thoughtbot.com/visual-ize-the-future
+export VISUAL="vim -p"
