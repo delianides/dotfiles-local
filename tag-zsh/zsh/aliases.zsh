38a29f59f0d4e074b223034ef8be29bf3a220b21
delianides <github@delianides.com>
2016-07-30 22:34:06 -0400

    load project nvmrc file

--- tag-zsh/zsh/aliases.zsh
+++ tag-zsh/zsh/aliases.zsh
@@ -70,3 +70,16 @@ alias cd....='cd ../../..'
 alias cd.....='cd ../../../..'
 alias cd/='cd /'
 
+# place this after nvm initialization!
+autoload -U add-zsh-hook
+load-nvmrc() {
+  if [[ -f .nvmrc && -r .nvmrc ]]; then
+    nvm use
+  elif [[ $(nvm version) != $(nvm version default)  ]]; then
+    echo "Reverting to nvm default version"
+    nvm use default
+  fi
+}
+add-zsh-hook chpwd load-nvmrc
+load-nvmrc
+
