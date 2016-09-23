7549117818070f37d4e680d0a48e7a64a347f436
delianides <github@delianides.com>
2016-09-23 13:46:01 -0400

    path changes

--- tag-zsh/zsh/path.zsh
+++ tag-zsh/zsh/path.zsh
@@ -5,17 +5,12 @@ export GOBIN="$HOME/Projects/go/bin"
 # Homebrew
 export PATH="/usr/local/bin:$PATH"
 
-# Heroku standalone client
-export PATH="/usr/local/heroku/bin:$PATH"
-
-# Node
-export PATH=$PATH:/usr/local/share/npm/bin
-
 export PATH=$HOME/.bin:$PATH
 
 export PATH="$PATH:$HOME/.rvm/bin:$GOBIN" # Add RVM to PATH for scripting
 
 export NVM_DIR="$HOME/.nvm"
+
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
 
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
